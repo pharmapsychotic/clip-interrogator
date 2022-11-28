f22be02819f5c70674563246c9fce753a433a56a
pharmapsychotic <me@pharmapsychotic.com>
2022-11-27 21:23:21 -0600

    Prep merge to main

--- predict.py
+++ predict.py
@@ -21,17 +21,22 @@ class Predictor(BasePredictor):
         image: Path = Input(description="Input image"),
         clip_model_name: str = Input(
             default="ViT-L-14/openai",
-            choices=[
-                "ViT-L-14/openai",
-                "ViT-H-14/laion2b_s32b_b79k",
-            ],
-            description="Choose a clip model.",
+            choices=["ViT-L-14/openai", "ViT-H-14/laion2b_s32b_b79k"],
+            description="Choose ViT-L for Stable Diffusion 1, and ViT-H for Stable Diffusion 2",
+        ),        
+        mode: str = Input(
+            default="best",
+            choices=["best", "fast"],
+            description="Prompt mode (best takes 10-20 seconds, fast takes 1-2 seconds).",
         ),        
     ) -> str:
         """Run a single prediction on the model"""
         image = Image.open(str(image)).convert("RGB")
         self.switch_model(clip_model_name)
-        return self.ci.interrogate(image)
+        if mode == "best":
+            return self.ci.interrogate(image)
+        else:
+            return self.ci.interrogate_fast(image)
     
     def switch_model(self, clip_model_name: str):
         if clip_model_name != self.ci.config.clip_model_name:

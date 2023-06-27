21392325e0a37ee4c367074d17d3c317f6ffd6b9
Ben Cherry <bcherry@gmail.com>
2023-06-26 17:02:44 -0700

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import sys
-from PIL import Image
-from clip_interrogator import Config, Interrogator
-# import torch
-image = Image.open('../pushd-gpt/output/resized.jpg').convert('RGB')
-ci = Interrogator(Config(clip_model_name="ViT-H-14/laion2b_s32b_b79k", device='mps'))
-print(ci.interrogate(image))
